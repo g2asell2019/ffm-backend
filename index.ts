6b43a0938277d0ecd13e1621076b451e4f6b7f7e
NAD-2K3 <anhduc19112003@gmail.com>
2023-11-03 17:38:45 +0700

    test    1

--- index.ts
+++ index.ts
@@ -2,8 +2,7 @@ import { connect } from "@planetscale/database";
 import "dotenv/config";
 import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
-import * as schema from './drizzle/database'
-import * as model from './model/model'
+import * as model from './model/model';
 
 
 const conn = connect({
@@ -14,6 +13,6 @@ const db = drizzle(conn);
 const statement = sql`select * from Customer `;
 db.execute(statement).then((value) => {
   const customer: model.Customer[] = value.rows as model.Customer[];
-  
+
   console.log(customer[0].Name);
 });
