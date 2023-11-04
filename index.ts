81b75060b049efa55eba45aaf05ffba7621afd30
g2asell2019 <51025883+g2asell2019@users.noreply.github.com>
2023-11-04 22:11:52 +0700

    remove test query

--- index.ts
+++ index.ts
@@ -1,8 +1,2 @@
 import "dotenv/config";
 
-const statement = sql`select * from Customer `;
-db.execute(statement).then((value) => {
-  const customer: model.Customer[] = value.rows as model.Customer[];
-  
-  console.log(customer[0].Name);
-});
