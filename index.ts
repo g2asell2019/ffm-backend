fe1e190c1c588a7b526eeb24b217d227f06f04ce
g2asell2019 <51025883+g2asell2019@users.noreply.github.com>
2023-11-04 22:13:37 +0700

    setup for express

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
 import "dotenv/config";
+import express, { Router } from "express";
+import 'express-async-errors';
+import bodyParser from "body-parser";
 
+const app = express();
+const port = process.env.PORT || 3030;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.set('json spaces', '  ')
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
